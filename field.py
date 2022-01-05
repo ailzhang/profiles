6d78e94c1a513dada17612f0561a6d351f4b3419
Ailing Zhang <ailing@taichi.graphics>
2022-01-05 18:48:23 +0800

    Recover p2g kernel
    C++ field 50us, ndarray 70 us.
    In Nsight profiler: field 0.04ms, ndarray 0.05ms
    Need to increse workload

--- field.py
+++ field.py
@@ -8,7 +8,8 @@ ti.init(arch=ti.opengl, log_level=ti.TRACE, allow_nv_shader_extension=False, use
 
 #ti.set_logging_level(ti.TRACE)
 
-n_particles = 64 * 64 * 2
+N = 64
+n_particles = N * N * 2
 n_grid = 128
 dx = 1 / n_grid
 dt = 2e-4
@@ -31,23 +32,22 @@ grid_m = ti.field(float, (n_grid, n_grid))
 
 @ti.kernel
 def substep():
-    pass
-    #for i, j in grid_m:
-    #    grid_v[i, j] = [0, 0]
-    #    grid_m[i, j] = 0
-    #for p in x:
-    #    Xp = x[p] / dx
-    #    base = int(Xp - 0.5)
-    #    fx = Xp - base
-    #    w = [0.5 * (1.5 - fx)**2, 0.75 - (fx - 1)**2, 0.5 * (fx - 0.5)**2]
-    #    stress = -dt * 4 * E * p_vol * (J[p] - 1) / dx**2
-    #    affine = ti.Matrix([[stress, 0], [0, stress]]) + p_mass * C[p]
-    #    for i, j in ti.static(ti.ndrange(3, 3)):
-    #        offset = ti.Vector([i, j])
-    #        dpos = (offset - fx) * dx
-    #        weight = w[i].x * w[j].y
-    #        grid_v[base + offset] += weight * (p_mass * v[p] + affine @ dpos)
-    #        grid_m[base + offset] += weight * p_mass
+    for i, j in grid_m:
+        grid_v[i, j] = [0, 0]
+        grid_m[i, j] = 0
+    for p in x:
+        Xp = x[p] / dx
+        base = int(Xp - 0.5)
+        fx = Xp - base
+        w = [0.5 * (1.5 - fx)**2, 0.75 - (fx - 1)**2, 0.5 * (fx - 0.5)**2]
+        stress = -dt * 4 * E * p_vol * (J[p] - 1) / dx**2
+        affine = ti.Matrix([[stress, 0], [0, stress]]) + p_mass * C[p]
+        for i, j in ti.static(ti.ndrange(3, 3)):
+            offset = ti.Vector([i, j])
+            dpos = (offset - fx) * dx
+            weight = w[i].x * w[j].y
+            grid_v[base + offset] += weight * (p_mass * v[p] + affine @ dpos)
+            grid_m[base + offset] += weight * p_mass
 
 @ti.kernel
 def init():
@@ -79,7 +79,7 @@ def run_steps(n=500):
         substep()
 
 def run():
-    # init()
+    init()
     gui = ti.GUI('MPM88')
     while gui.running and not gui.get_event(gui.ESCAPE):
         run_steps()
